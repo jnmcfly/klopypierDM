ec2f458f761570e0aee52e4966e3be5d5cd0995a
Jan <jan.bechtel@gmail.com>
2020-10-24 20:04:51 +0200

    sanitize

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 
+#shopId = "951" # schwalmstadt
+shopId = "2387"
+
 
 articles = ["595420", "708997", "137425", "28171", "485698", "799358", "863567", "452740", "610544", "846857", "709006", "452753", "879536", "452744", "485695", "853483", "594080", "504606", "593761", "525943", "842480", "535981", "127048", "524535"]
 headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/517.66 (KHTML, like Gecko) Chrome/79.0.3945.88"}
@@ -20,13 +23,12 @@ def getPaper(articles, shopId):
 def getStoreInfo(shopId):
     url = 'https://store-data-service.services.dmtech.com/stores/item/de/' + shopId
     req = requests.get(url)
-    print(req.text)
     info = req.json()["address"]["city"] + ", " + req.json()["address"]["street"]
 
     return info
 
-sum = getPaper(articles, "951")
-info = getStoreInfo("951")
+sum = getPaper(articles, shopId)
+info = getStoreInfo(shopId)
 
 print("Noch " + str(sum) + " Packungen Klopapier in Filiale " + info)
 
